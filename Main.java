d38a2515ea086b4228e2fd1108d7c0b56d0e767f
Denny <107827565+DennyApriadi@users.noreply.github.com>
2022-12-18 19:55:38 +0700

    update code

--- Main.java
+++ Main.java
@@ -16,26 +16,39 @@ public class Main {
         antrian.add("Sarip");
         antrian.add("Cindy");
 
-        System.out.print("\nDaftar Nama Antrian \t\t:" + antrian);
-        System.out.print("\n\nMemanggil Antrian (y/n) \t: ");
-        char hasil = input.next().charAt(0);
+        System.out.print("********************************************************************************");
+        System.out.print("\nDaftar Nama Antrian \t\t\t\t: " + antrian);
+        System.out.print("\n********************************************************************************");
 
-        if (hasil == 'y') {
-            int size = antrian.size();
-            for (int i=0; i < size; i++) {
-                String panggil = antrian.poll();
-                System.out.print("\nMemanggil Antrian \t\t\t:" + "[" + panggil + "]");
-                System.out.print("\nDaftar Antrian Selanjutnya \t:" + antrian + "\n");
-                if (antrian.size() == 0) {
-                    System.out.print("\n\t||| Antrian Kosong |||\n\n");
-                }
+        while (true) {
+            System.out.print("\n\nMemanggil Antrian Pertama (y/n)\t\t: ");
+            char hasil = input.next().charAt(0);
+
+            if (hasil == 'y') {
+                int size = antrian.size();
+                for (int i=0; i < size; i++) {
+                    String panggil = antrian.remove();
+                    System.out.print("Memanggil Antrian\t\t\t\t\t: " + "[" + panggil + "]");
+                    System.out.print("\nDaftar Antrian Selanjutnya \t\t\t: " + antrian + "\n");
+                    System.out.print("\nMemanggil Antrian Selanjutnya (y/n)\t: ");
+                    hasil = input.next().charAt(0);
+                    if (antrian.size() == 0) {
+                        System.out.print("\n\t||| Antrian Kosong |||\n\n");
+                    }
+                    else if ( hasil != 'y' ) {
+                        System.out.print("\n\nAntrian Yang Tersisa Adalah \t\t: " + antrian);
+                        System.out.println("\n\t   Program Berakhir\n\t+++Sistem Dimatikan+++\n\n");
+                        break;
+                    }
+                } break;
+            } else if (hasil == 'n') {
+                System.out.println("\n   Baik Terimakasi\n+++Sistem Dimatikan+++\n\n");
+                break;
+            } else {
+                System.out.println("\n||| Input Anda Salah |||\n\t+++Sistem Dimatikan+++\n\n");
+                break;
             }
-        } else if (hasil == 'n') {
-            System.out.println("\n\tBaik Terimakasi\n+++Sistem Dimatikan+++\n\n");
-        } else {
-            System.out.println("\n||| Input Anda Salah |||\n\n");
+
         }
-
-
     }
 }
